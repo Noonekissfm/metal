6fee86aa8abb156e18b483b85e23dd2b8ce9d87b
rnglol <createsite108@gmail.com>
2023-02-05 19:42:26 +0000

    style requests

--- src/components/ItemCard/components/Price/index.tsx
+++ src/components/ItemCard/components/Price/index.tsx
@@ -9,20 +9,26 @@ interface IProps {
 }
 
 export const Price: FC<IProps> = ({ price }) => {
-    const classList = ['wrapper', price? '--price' : ''].join('')
+    const classList = ['wrapper', price ? '--price' : ''].join('')
+
+    let discounted_price = null;
+    if (price) {
+        discounted_price = ((parseInt(price) * 0.98).toFixed(2)).toString();
+    }
+
     return (
         <div className={style[classList]}>
             {!!price
-            ?<>
-                <p>Стоимость одной тонны: <span>&nbsp;{price}&nbsp;</span>руб.</p>
-                <div className={style.order}>
-                    <p>Сделать заказ можно по телефону: &nbsp;</p>
-                    <PhoneLink />
-                </div>
-            </>
-            :<>
-                <p>Уточнить стоимость можно по телефону: &nbsp;</p><PhoneLink />
-            </>}
+                ? <>
+                    <p>Стоимость одной тонны: <span>&nbsp;{discounted_price}&nbsp;</span>руб.</p>
+                    <div className={style.order}>
+                        <p>Сделать заказ можно по телефону: &nbsp;</p>
+                        <PhoneLink />
+                    </div>
+                </>
+                : <>
+                    <p>Уточнить стоимость можно по телефону: &nbsp;</p><PhoneLink />
+                </>}
         </div>
     );
 };
